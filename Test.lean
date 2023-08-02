6fcb2f5f26c855ef88026cb3fa31d4c532d8ad8e
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2023-08-02 09:34:56 +0000

    some stronger normalization now seems necessary

--- Test.lean
+++ Test.lean
@@ -4,3 +4,13 @@ inductive True : Prop where
   | intro : True
 
 def test : True → True := λ x : True => x
+
+inductive Nat : Type where
+  | zero : Nat
+  | succ : Nat → Nat
+
+noncomputable def Nat.add (a : Nat) (b : Nat) : Nat :=
+  Nat.rec a (fun _ sum => Nat.succ sum) b
+
+inductive Eq : α → α → Prop where
+  | refl (a : α) : Eq a a
