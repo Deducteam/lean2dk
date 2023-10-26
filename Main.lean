534c57a68fc6bc4169faafcf63b044703f0b045d
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2023-10-26 06:24:25 +0000

    ensure that normalize module is loaded

--- Main.lean
+++ Main.lean
@@ -33,7 +33,8 @@ def main (args : List String) : IO UInt32 := do
   let dkEnvString ← match (ExceptT.run (StateT.run (ReaderT.run (dkEnv.print) {env := dkEnv}) default)) with
     | .error s => throw $ IO.userError s
     | .ok (_, s) => pure $ "\n\n".intercalate s.out
-  let dkEnvString := dkEnvString ++ "\n"
+  let dkPrelude := "#REQUIRE normalize.\n"
+  let dkEnvString := dkPrelude ++ dkEnvString ++ "\n"
 
   IO.FS.writeFile "dk/out.dk" dkEnvString
 
